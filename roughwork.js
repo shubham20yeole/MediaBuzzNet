265637653993e4c976df457b29a8eae0fe506559
Shubham yeole <shubham20.yeole@gmail.com>
2017-01-25 15:11:03 -0500

    Update Project List and GPS functionality

--- roughwork.js
+++ roughwork.js
@@ -7,30 +7,16 @@ var path = require('path');
 var expressValidator = require('express-validator');
 var mongojs = require('mongojs')
 var mongodb = require('mongodb')
-var collections = ["users", "blog", "comments", "property", "images", "notification", "bookmark", "messages","timetable", "timetablecategory", "timetablequestion", "resume", "skills", "locations"]
-var db = mongojs('mongodb://*************:*************@*************.mlab.com:*************/*************', collections)
+var collections = ["locations", "documentscategory", "documents"]
+var db = mongojs('mongodb://********:***********@ds163387.mlab.com:*******/***********', collections)
 var app = express();
 var ObjectId = mongojs.ObjectId;
 var passport = require("passport")
 var JSFtp = require("jsftp");
 var fs = require('fs');
 var config = {
-  host: 'ftp.*************.com',
+  host: 'ftp.******.com',
   port: 21,
-  user: '*************',
+  user: '**********',
   password: '*************'
 }
-
-          <span id="selCatSelect">
-            <select class="select">
-              <option value="default">Select Category</option>
-                <% documentscategory.forEach(function(documentscategory){%>
-                  <option value="<%= documentscategory.catname %>"><%= documentscategory.catname %></option>
-                <%})%>
-            </select>
-          </span>
-          <button type="button" id="addCatButton" class="btn">Add New Category</button>
-
-
-          <span id="addCatInput"><input type="text" name="category" id="cat-write" placeholder="Enter category name" autocomplete="off"></span>
-          <button type="button" id="selCatButton" class="btn">Select Category</button>
